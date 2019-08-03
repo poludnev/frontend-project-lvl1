386f3118da8efb89d5672e18428a30301db7dba8
POLUDNEV <poludnev.roman@gmail.com>
2019-08-03 22:13:35 +1000

    lesson3

---
+++ dist/bin/brain-games.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+"use strict";
+
+var _ = _interopRequireDefault(require(".."));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log('Welcome to the Brain Games!');
+(0, _.default)();
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUNBOzs7O0FBRUFBLE9BQU8sQ0FBQ0MsR0FBUixDQUFZLDZCQUFaO0FBRUEiLCJzb3VyY2VzQ29udGVudCI6WyIjIS91c3IvYmluL2VudiBub2RlXG5pbXBvcnQgc3RHcmVldCBmcm9tICcuLic7XG5cbmNvbnNvbGUubG9nKCdXZWxjb21lIHRvIHRoZSBCcmFpbiBHYW1lcyEnKTtcblxuc3RHcmVldCgpO1xuXG4iXX0=
