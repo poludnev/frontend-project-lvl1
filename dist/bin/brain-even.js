525792f9b51a3019a8fe37569f6843261a7efb33
POLUDNEV <poludnev.roman@gmail.com>
2019-09-01 12:34:20 +0800

    01092019

--- dist/bin/brain-even.js
+++ dist/bin/brain-even.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
-// import greeting from '..';
 "use strict";
 
 var _ = require("..");
 
-console.log('Welcome to the Brain Even Games!');
-console.log('Answer \x1b[31m"yes"\x1b[0m \x1b[1mif\x1b[22m number even otherwise answer \x1b[31m"no"\x1b[0m.');
-(0, _.evenGame)((0, _.greeting)());
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZXZlbi5qcyJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sIm1hcHBpbmdzIjoiQUFBQTtBQUNBOzs7QUFDQTs7QUFHQUEsT0FBTyxDQUFDQyxHQUFSLENBQVksa0NBQVo7QUFDQUQsT0FBTyxDQUFDQyxHQUFSLENBQVksaUdBQVo7QUFFQSxnQkFBUyxpQkFBVCIsInNvdXJjZXNDb250ZW50IjpbIiMhL3Vzci9iaW4vZW52IG5vZGVcbi8vIGltcG9ydCBncmVldGluZyBmcm9tICcuLic7XG5pbXBvcnQgeyBldmVuR2FtZSwgZ3JlZXRpbmcgfSBmcm9tICcuLic7XG5cblxuY29uc29sZS5sb2coJ1dlbGNvbWUgdG8gdGhlIEJyYWluIEV2ZW4gR2FtZXMhJyk7XG5jb25zb2xlLmxvZygnQW5zd2VyIFxceDFiWzMxbVwieWVzXCJcXHgxYlswbSBcXHgxYlsxbWlmXFx4MWJbMjJtIG51bWJlciBldmVuIG90aGVyd2lzZSBhbnN3ZXIgXFx4MWJbMzFtXCJub1wiXFx4MWJbMG0uJyk7XG5cbmV2ZW5HYW1lKGdyZWV0aW5nKCkpO1xuIl19
+console.log('Welcome to the Brain Even Games! \nAnswer \x1b[31m"yes"\x1b[0m \x1b[1mif\x1b[22m number even otherwise, answer \x1b[31m"no"\x1b[0m.\n');
+(0, _.brainGame)(3, 'even');
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZXZlbi5qcyJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sIm1hcHBpbmdzIjoiQUFBQTs7O0FBQ0E7O0FBRUFBLE9BQU8sQ0FBQ0MsR0FBUixDQUFZLHVJQUFaO0FBRUEsaUJBQVUsQ0FBVixFQUFhLE1BQWIiLCJzb3VyY2VzQ29udGVudCI6WyIjIS91c3IvYmluL2VudiBub2RlXG5pbXBvcnQgeyBldmVuR2FtZSwgZ3JlZXRpbmcsIGJyYWluR2FtZSB9IGZyb20gJy4uJztcblxuY29uc29sZS5sb2coJ1dlbGNvbWUgdG8gdGhlIEJyYWluIEV2ZW4gR2FtZXMhIFxcbkFuc3dlciBcXHgxYlszMW1cInllc1wiXFx4MWJbMG0gXFx4MWJbMW1pZlxceDFiWzIybSBudW1iZXIgZXZlbiBvdGhlcndpc2UsIGFuc3dlciBcXHgxYlszMW1cIm5vXCJcXHgxYlswbS5cXG4nKTtcblxuYnJhaW5HYW1lKDMsICdldmVuJyk7Il19
--- dist/bin/brain-games.js
+++ dist/bin/brain-games.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 "use strict";
 
-var _ = _interopRequireDefault(require(".."));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _ = require("..");
 
 console.log('Welcome to the Brain Games!');
-(0, _.default)();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUNBOzs7O0FBRUFBLE9BQU8sQ0FBQ0MsR0FBUixDQUFZLDZCQUFaO0FBRUEiLCJzb3VyY2VzQ29udGVudCI6WyIjIS91c3IvYmluL2VudiBub2RlXG5pbXBvcnQgZ3JlZXRpbmcgZnJvbSAnLi4nO1xuXG5jb25zb2xlLmxvZygnV2VsY29tZSB0byB0aGUgQnJhaW4gR2FtZXMhJyk7XG5cbmdyZWV0aW5nKCk7XG4iXX0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUNBOztBQUVBQSxPQUFPLENBQUNDLEdBQVIsQ0FBWSw2QkFBWiIsInNvdXJjZXNDb250ZW50IjpbIiMhL3Vzci9iaW4vZW52IG5vZGVcbmltcG9ydCB7IGdyZWV0aW5nLCBnYW1lMX0gZnJvbSAnLi4nO1xuXG5jb25zb2xlLmxvZygnV2VsY29tZSB0byB0aGUgQnJhaW4gR2FtZXMhJyk7XG5cbiJdfQ==
