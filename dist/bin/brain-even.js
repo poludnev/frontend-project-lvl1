f1639b86d12f8f5ff2a020673d286b35d107b613
poludnev <51874367+poludnev@users.noreply.github.com>
2019-09-03 17:13:39 +0800

    Delete brain-even.js

--- dist/bin/brain-even.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-
-var _ = _interopRequireDefault(require(".."));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-(0, _.default)('even');
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZXZlbi5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTs7O0FBQ0E7Ozs7QUFDQSxlQUFhLE1BQWIiLCJzb3VyY2VzQ29udGVudCI6WyIjIS91c3IvYmluL2VudiBub2RlXG5pbXBvcnQgYnJhaW5HYW1lTmV3IGZyb20gJy4uJ1xuYnJhaW5HYW1lTmV3KCdldmVuJyk7XG4iXX0=
