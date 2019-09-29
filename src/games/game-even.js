117aed493ac3db3a28b58100797a65848fc998f5
POLUDNEV <poludnev.roman@gmail.com>
2019-09-29 22:39:59 +1000

    keywords, access point, dependences
    keywords revised to array
    access point removed
    dependencies and devdepends rearranged

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -5,6 +5,6 @@ import randNum from '../modules/random-number';
 const isEven = (num) => (num % 2 === 0);
 export default () => {
   const intro = `Answer ${decorate('"yes"')} \x1b[1mif\x1b[22m number even otherwise, answer ${decorate('"no"')}.\n`;
-  const questNum = randNum();
-  return cons(intro, cons(questNum, (isEven(questNum) ? 'yes' : 'no')));
+  const question = randNum();
+  return cons(intro, cons(question, (isEven(question) ? 'yes' : 'no')));
 };
