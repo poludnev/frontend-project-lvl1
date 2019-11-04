c78a9b85f99f52d9e948528b8e57864e8f4a2dd7
POLUDNEV <poludnev.roman@gmail.com>
2019-11-04 16:48:02 +1000

    Nov/04/19
    Added new abstract for GCD

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -1,28 +1,26 @@
 import { cons } from '@hexlet/pairs';
 import randNum from '../modules/random-number';
 
-export default () => {
-  const intro = 'Find the greatest common divisor of given numbers.\n';
 
-  const number1 = randNum();
-  const number2 = randNum();
-
-  let result = 0;
+const getGCD = (number1, number2) => {
+  let result = 1;
   const questExpression = `${number1} ${number2}`;
-
-  if (number1 === 0 || number2 === 0) {
-    result = 1;
-    return cons(intro, cons(questExpression, result));
-  }
-
-  let mediateNumber1 = number1;
-  let mediateNumber2 = number2;
-  if (number1 < number2) { mediateNumber1 = number2; mediateNumber2 = number1; }
-  for (let devisor = mediateNumber2; devisor >= 0; devisor -= 1) {
-    if (mediateNumber1 % devisor === 0 && mediateNumber2 % devisor === 0) {
-      result = devisor;
-      return cons(intro, cons(questExpression, result));
+  if (number1 > 0 && number2 > 0) {
+    let mediateNumber1 = number1;
+    let mediateNumber2 = number2;
+    if (number1 < number2) { mediateNumber1 = number2; mediateNumber2 = number1; }
+    for (let devisor = mediateNumber2; devisor >= 0; devisor -= 1) {
+      if (mediateNumber1 % devisor === 0 && mediateNumber2 % devisor === 0) {
+        result = devisor;
+        return cons(questExpression, result);
+      }
     }
   }
-  return null;
+  return cons(questExpression, result);
+};
+
+
+export default () => {
+  const intro = 'Find the greatest common divisor of given numbers.\n';
+  return cons(intro, getGCD(randNum(), randNum()));
 };
