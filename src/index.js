d8219d51692bc8d7f3142698e2d2c36b63f9400b
POLUDNEV <poludnev.roman@gmail.com>
2020-04-13 00:00:02 +1000

    1. Modified the "intro' process: getting from game and decodating. 2. Removed the toString() for answer is got from game

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 import readlineSync from 'readline-sync';
 import { car, cdr } from '@hexlet/pairs';
-import { getDecoratedString, decorate } from './modules/decorate';
+import { getDecoratedString, decorate } from './utils/funcs';
 
 export default (game) => {
   const attemptsLimit = 3;
   console.log('Welcome to the Brain Games!');
-  console.log(getDecoratedString(car(game()))); // show new intro;
+  const intro = car(game());
+
+  const decorStyle = [
+    { word: '"yes"', style: 'red' },
+    { word: '"no".', style: 'red' },
+  ];
+  const decoratedIntro = getDecoratedString(intro, decorStyle);
+
+  console.log(`${decoratedIntro}\n`);
+
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   for (let i = 0; i < attemptsLimit; i += 1) {
     const gameCall = game();
-    console.log(`Question: ${car(cdr(gameCall))}`);
+    const question = car(cdr(gameCall));
+    console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
     const result = cdr(cdr(gameCall));
-    if (answer !== result.toString()) {
+    if (answer !== result) {
       console.log(`${decorate(['\'', answer, '\''], 'red')} is wrong answer, correct answer was ${decorate(['\'', result, '\''], 'red')}.`);
       console.log(`Let${decorate(['\'s try again,'], 'red')} ${decorate([name, '!'], 'red')}`);
       return;
